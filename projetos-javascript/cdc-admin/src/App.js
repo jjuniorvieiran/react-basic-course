1d1115859a4fb203dddfe458e5c12927a1fd4739
Júnior <junior.jaava@gmail.com>
2019-06-09 13:47:37 +0200

    Handling with component state; using ajax; consuming rest api

--- projetos-javascript/cdc-admin/src/App.js
+++ projetos-javascript/cdc-admin/src/App.js
@@ -1,71 +1,98 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
+import $ from 'jquery';
 
-function App() {
-  return (
-    <div id="layout">
-      <a href="#menu" id="menuLink" className="menu-link">
-        <span></span>
-      </a>
-      <div id="menu">
-        <div className="pure-menu">
-          <a className="pure-menu-heading" href="#">Company</a>
+class App extends Component {
 
-          <ul className="pure-menu-list">
-            <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
-            <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
-            <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
-          </ul>
-        </div>
-      </div>
+  constructor() {
+    super();
+    this.state = { lista: [] };
+  }
 
-      <div id="main">
-        <div className="header">
-          <h1>Cadastro de Autores</h1>
-        </div>
-        <div className="content" id="content">
-          <div className="pure-form pure-form-aligned">
-            <form className="pure-form pure-form-aligned">
-              <div className="pure-control-group">
-                <label htmlFor="nome">Nome</label>
-                <input id="nome" type="text" name="nome" value="" />
-              </div>
-              <div className="pure-control-group">
-                <label htmlFor="email">Email</label>
-                <input id="email" type="email" name="email" value="" />
-              </div>
-              <div className="pure-control-group">
-                <label htmlFor="senha">Senha</label>
-                <input id="senha" type="password" name="senha" />
-              </div>
-              <div className="pure-control-group">
-                <label></label>
-                <button type="submit" className="pure-button pure-button-primary">Gravar</button>
-              </div>
-            </form>
+  //we could put this code on constructor but wouldnt be a good practice. It is much better respect life cycle of instancialization
+  componentWillMount() {
+    $.ajax({
+      url: "http://localhost:8080/api/autores",
+      dataType: 'json',
+      success: function (resposta) {
+        console.log(this);
+        this.setState({lista:resposta}); //everytime setState is called, render will be executed.
+      }.bind(this) //the 'this' inside this function is the this from App object, not this of jquery
+    }
+    );
+  }
 
-          </div>
-          <div>
-            <table className="pure-table">
-              <thead>
-                <tr>
-                  <th>Nome</th>
-                  <th>email</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Alberto</td>
-                  <td>alberto.souza@caelum.com.br</td>
-                </tr>
-              </tbody>
-            </table>
+  render() {
+    return (
+      <div id="layout">
+        <a href="#menu" id="menuLink" className="menu-link">
+          <span></span>
+        </a>
+        <div id="menu">
+          <div className="pure-menu">
+            <a className="pure-menu-heading" href="#">Company</a>
+
+            <ul className="pure-menu-list">
+              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
+              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
+              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
+            </ul>
           </div>
         </div>
+
+        <div id="main">
+          <div className="header">
+            <h1>Cadastro de Autores</h1>
+          </div>
+          <div className="content" id="content">
+            <div className="pure-form pure-form-aligned">
+              <form className="pure-form pure-form-aligned">
+                <div className="pure-control-group">
+                  <label htmlFor="nome">Nome</label>
+                  <input id="nome" type="text" name="nome" value="" />
+                </div>
+                <div className="pure-control-group">
+                  <label htmlFor="email">Email</label>
+                  <input id="email" type="email" name="email" value="" />
+                </div>
+                <div className="pure-control-group">
+                  <label htmlFor="senha">Senha</label>
+                  <input id="senha" type="password" name="senha" />
+                </div>
+                <div className="pure-control-group">
+                  <label></label>
+                  <button type="submit" className="pure-button pure-button-primary">Gravar</button>
+                </div>
+              </form>
+
+            </div>
+            <div>
+              <table className="pure-table">
+                <thead>
+                  <tr>
+                    <th>Nome</th>
+                    <th>email</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    this.state.lista.map(function (autor) {
+                      return (
+                        <tr>
+                          <td>{autor.nome}</td>
+                          <td>{autor.email}</td>
+                        </tr>
+                      );
+                    })
+                  }
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
 export default App;

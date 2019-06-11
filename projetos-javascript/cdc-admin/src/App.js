7d17dbafc0dc04ccd461d7f551e87666e7dc7ce8
Júnior <junior.jaava@gmail.com>
2019-06-11 17:13:22 +0200

    -Refactoring App.js in more components

--- projetos-javascript/cdc-admin/src/App.js
+++ projetos-javascript/cdc-admin/src/App.js
@@ -1,62 +1,9 @@
 import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
-import $ from 'jquery';
-import InputCustomizado from './componentes/InputCustomizado';
+import {FormularioAutor,TabelaAutores} from './Autor';
 
 class App extends Component {
-
-  constructor() {
-    super();
-    this.state = { lista: [], nome: '', email: '', senha: '' };
-    this.enviaForm = this.enviaForm.bind(this);
-    this.setNome = this.setNome.bind(this);
-    this.setEmail = this.setEmail.bind(this);
-    this.setSenha = this.setSenha.bind(this);
-  }
-
-  //we could put this code on constructor but wouldnt be a good practice. It is much better respect life cycle of instancialization
-  componentDidMount() {
-    $.ajax({
-      url: "http://localhost:8080/api/autores",
-      dataType: 'json',
-      success: function (resposta) {
-        this.setState({ lista: resposta }); //everytime setState is called, render will be executed.
-      }.bind(this) //the 'this' inside this function is the this from App object, not this of jquery
-    }
-    );
-  }
-
-  enviaForm(evento) {
-    evento.preventDefault(); //for do not allow reload page
-
-    $.ajax({
-      url: "http://localhost:8080/api/autores",
-      contentType: 'application/json',
-      dataType: 'json',
-      type: 'post',
-      data: JSON.stringify({ nome: this.state.nome, email: this.state.email, senha: this.state.senha }),
-      success: function (resposta) {
-        console.log("enviado com sucesso");
-        this.setState({ lista: resposta }); // to reload the page, only think that we need to do is update setState 
-      }.bind(this), //this code means, the this is related to the class is not related to the jquery.
-      error: function (resposta) {
-        console.log("erro");
-      }
-    });
-  }
-
-  setNome(evento) {
-    this.setState({ nome: evento.target.value });
-  }
-
-  setEmail(evento) {
-    this.setState({ email: evento.target.value });
-  }
-
-  setSenha(evento) {
-    this.setState({ senha: evento.target.value });
-  }
 
   render() {
     return (
@@ -81,40 +28,8 @@ class App extends Component {
             <h1>Cadastro de Autores</h1>
           </div>
           <div className="content" id="content">
-            <div className="pure-form pure-form-aligned">
-              <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm.bind(this)} method="post">
-                <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome" />
-                <InputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="Email" />
-                <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha" />
-                <div className="pure-control-group">
-                  <label></label>
-                  <button type="submit" className="pure-button pure-button-primary">Gravar</button>
-                </div>
-              </form>
-
-            </div>
-            <div>
-              <table className="pure-table">
-                <thead>
-                  <tr>
-                    <th>Nome</th>
-                    <th>email</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {
-                    this.state.lista.map(function (autor) {
-                      return (
-                        <tr key={autor.id}>
-                          <td>{autor.nome}</td>
-                          <td>{autor.email}</td>
-                        </tr>
-                      );
-                    })
-                  }
-                </tbody>
-              </table>
-            </div>
+            <FormularioAutor/>
+            <TabelaAutores/>
           </div>
         </div>
       </div>

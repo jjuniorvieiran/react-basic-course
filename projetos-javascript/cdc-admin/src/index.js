115024fe892c2fd18e92e765bf23e3e972f36175
Júnior <junior.jaava@gmail.com>
2019-06-16 18:57:31 +0200

    Starting to work with routers

--- projetos-javascript/cdc-admin/src/index.js
+++ projetos-javascript/cdc-admin/src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from 'reducers/reducer.js'
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(
+  <Provider store={store}>
+    <Router>
+      <Route path='/' component={App} />
+      <Route path='/path' />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+)

55bcaeb3f2333f953759b8ac2589f28810580d08
Júnior <junior.jaava@gmail.com>
2019-06-17 15:01:23 +0200

    Starting to work with  browser history and link from react-router-dom

--- projetos-javascript/cdc-admin/src/index.js
+++ projetos-javascript/cdc-admin/src/index.js
@@ -1,19 +1,24 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import rootReducer from 'reducers/reducer.js'
-import App from './App'
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { createBrowserHistory } from "history";
+import rootReducer from 'reducers/reducer.js';
+import App from './App'; 
+import AutorBox from "./Autor";
 
+
+const history = createBrowserHistory()
 const store = createStore(rootReducer)
 
 render(
-  <Provider store={store}>
-    <Router>
-      <Route path='/' component={App} />
-      <Route path='/path' />
-    </Router>
-  </Provider>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <Router history={history}>
+            <Route path='/' component={App} />
+            <Route path='/autor' component={AutorBox}/>
+            <Route path='/livro' />
+        </Router>
+    </Provider>,
+    document.getElementById('root')
 )
